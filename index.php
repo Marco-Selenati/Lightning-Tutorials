5f5aaa92fb32a8177bf659505d4c79f89821cde6
Herrald <pascha2525@aol.ch>
2018-01-05 20:54:24 +0100

    Added cards

--- index.php
+++ index.php
@@ -1,48 +1,53 @@
 <?php
 $content = <<<'EOD'
 <h2>HTML</h2>
-          <p>
-              HTML is .
-          </p>
-          <p>
-            <ul>
-              <li>
-                <a href="sites/site1.php">basic struct</a>
-              </li>
-              <li>
-                <a href="#">basic struct</a>
-              </li>
-              <li>
-                <a href="#">basic struct</a>
-              </li>
-              <li>
-                <a href="#">basic struct</a>
-              </li>
-            </ul>
-          </p>
+<div class="row">
+    <div class="card">
+      <div class="card-block">
+        <h4 class="card-title"> </h4>
+        <p class="card-text">ssss<p>
+        <a href="sites/site1.php" class="btn btn-primary">asdf</a>
+      </div>
+    </div>
+  </a>
+</div>
+<h2>HTML</h2>
+<p>
+  <ul>
+    <li>
+      <a href="sites/site1.php">basic struct</a>
+    </li>
+    <li>
+      <a href="#">basic struct</a>
+    </li>
+    <li>
+      <a href="#">basic struct</a>
+    </li>
+    <li>
+      <a href="#">basic struct</a>
+    </li>
+  </ul>
+</p>
 <pre><code class="html">
 EOD;
 $content .= htmlspecialchars(<<<'EOD'
 <h2>HTML</h2>
-          <p>
-              HTML is .
-          </p>
-          <p>
-            <ul>
-              <li>
-                <a href="#">basic struct</a>
-              </li>
-              <li>
-                <a href="#">basic struct</a>
-              </li>
-              <li>
-                <a href="#">basic struct</a>
-              </li>
-              <li>
-                <a href="#">basic struct</a>
-              </li>
-            </ul>
-          </p>
+<p>
+  <ul>
+    <li>
+      <a href="sites/site1.php">basic struct</a>
+    </li>
+    <li>
+      <a href="#">basic struct</a>
+    </li>
+    <li>
+      <a href="#">basic struct</a>
+    </li>
+    <li>
+      <a href="#">basic struct</a>
+    </li>
+  </ul>
+</p>
 EOD
 );
 $content .= <<<'EOD'

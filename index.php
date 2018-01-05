c0798efee546ed290aa3d47c1c2c28936674d036
Herrald <pascha2525@aol.ch>
2018-01-05 21:16:38 +0100

    Multiple cards

--- index.php
+++ index.php
@@ -2,32 +2,30 @@
 $content = <<<'EOD'
 <h2>HTML</h2>
 <div class="row">
-    <div class="card">
-      <div class="card-block">
-        <h4 class="card-title"> </h4>
-        <p class="card-text">ssss<p>
-        <a href="sites/site1.php" class="btn btn-primary">asdf</a>
-      </div>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">HTML Foundation</h4>
+      <p class="card-text">Doctype!?</p>
+      <a href="sites/site1.php" class="btn btn-primary">Learn !</a>
     </div>
-  </a>
+  </div>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">HTML Foundation</h4>
+      <p class="card-text">Doctype!?</p>
+      <a href="sites/site1.php" class="btn btn-primary">Learn !</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">HTML Foundation</h4>
+      <p class="card-text">Doctype!?</p>
+      <a href="sites/site1.php" class="btn btn-primary">Learn !</a>
+    </div>
+  </div>
 </div>
-<h2>HTML</h2>
-<p>
-  <ul>
-    <li>
-      <a href="sites/site1.php">basic struct</a>
-    </li>
-    <li>
-      <a href="#">basic struct</a>
-    </li>
-    <li>
-      <a href="#">basic struct</a>
-    </li>
-    <li>
-      <a href="#">basic struct</a>
-    </li>
-  </ul>
-</p>
+EOD;
+$content .= <<<'EOD'
 <pre><code class="html">
 EOD;
 $content .= htmlspecialchars(<<<'EOD'

ef002d5a41815785f05a040ce9b40918b24dd136
Herrald <pascha2525@aol.ch>
2018-01-10 09:27:40 +0100

    Added site 2 generation

--- sites/site2.php
+++ sites/site2.php
@@ -1,31 +1,32 @@
 <?php
-require '../php/code.php';
-$content = <<<'EOD'
+require "../php/code.php";
+$content = "";
+$content .= <<<'EOD'
 <h1>HTML-Dokument</h1>
 <p class="lead">
   Aufbau eines HTML-Dokuments
 </p>
-EOD;
 
+EOD;
 $content .= format_code_inline_p(<<<'EOD'
 Jedes HTML-Dokument sollte mit der Zeile
 <!doctype html>
 beginnen, die dem Browser sagt, dass dieses Dokument vom Typ HMTL ist. Danach folgen die html Start und Endtags.
+
 EOD
 );
-
 $content .= format_code_block_p(<<<'EOD'
 <!doctype html>
 <html>
 </html>
+
 EOD
 );
-
 $content .= format_code_inline_p(<<<'EOD'
 In diese hinein kommen der head und der body
+
 EOD
 );
-
 $content .= format_code_block_p(<<<'EOD'
 <!doctype html>
 <html>
@@ -34,14 +35,14 @@ $content .= format_code_block_p(<<<'EOD'
   <body>
   </body>
 </html>
+
 EOD
 );
-
 $content .= format_code_inline_p(<<<'EOD'
 Im head werden gewisse Informationen die für die ganze Seite gelten gespeichert, wie zum Beispiel der <title>, oder die Kodierung. In den body kommt der eigentliche Inhalt der Seite, also alle <h1> und <p> Tags.
+
 EOD
 );
-
 $content .= format_code_block_p(<<<'EOD'
 <!doctype html>
 <html>
@@ -51,14 +52,13 @@ $content .= format_code_block_p(<<<'EOD'
   <body>
   </body>
 </html>
+
 EOD
 );
-
 $content .= format_code_inline_p(<<<'EOD'
 Dieser <meta> Tag beinhaltet mehrere neue Sachen. Zuerst einmal fällt auf das er keinen Endtag hat. Der meta Tag ist einer von ganz wenigen Tags die keinen Endtag haben, weil sie nicht da sind um ein Stück Text zu beschreiben, sondern einfach irgendetwas aussagen. In diesem Fall sagt uns der Meta-Tag, mit welcher Kodierung dieses Dokument kodiert wurde. UTF-8 ist heutzutage der Standard und deshalb benutzen wir diese Kodierung. Diese Kodierung wird mithilfe eines Attributs mit dem Namen charset übergeben. Grundsätzlich kann jeder Tag ein Attribut haben, aber es macht nicht bei allen Sinn.
 EOD
 );
-
 $rootoff = "../";
-require '../php/base.php';
+require "../php/base.php";
 ?>
---
+++ sites_content/site2.c
@@ -0,0 +1,36 @@
+_HTML___
+<h1>HTML-Dokument</h1>
+<p class="lead">
+  Aufbau eines HTML-Dokuments
+</p>
+ayylmao_CODE___
+Jedes HTML-Dokument sollte mit der Zeile
+<!doctype html>
+beginnen, die dem Browser sagt, dass dieses Dokument vom Typ HMTL ist. Danach folgen die html Start und Endtags.
+ayylmao_BCODE__
+<!doctype html>
+<html>
+</html>
+ayylmao_CODE___
+In diese hinein kommen der head und der body
+ayylmao_BCODE__
+<!doctype html>
+<html>
+  <head>
+  </head>
+  <body>
+  </body>
+</html>
+ayylmao_CODE___
+Im head werden gewisse Informationen die für die ganze Seite gelten gespeichert, wie zum Beispiel der <title>, oder die Kodierung. In den body kommt der eigentliche Inhalt der Seite, also alle <h1> und <p> Tags.
+ayylmao_BCODE__
+<!doctype html>
+<html>
+  <head>
+    <meta charset=”utf-8”>
+  </head>
+  <body>
+  </body>
+</html>
+ayylmao_CODE___
+Dieser <meta> Tag beinhaltet mehrere neue Sachen. Zuerst einmal fällt auf das er keinen Endtag hat. Der meta Tag ist einer von ganz wenigen Tags die keinen Endtag haben, weil sie nicht da sind um ein Stück Text zu beschreiben, sondern einfach irgendetwas aussagen. In diesem Fall sagt uns der Meta-Tag, mit welcher Kodierung dieses Dokument kodiert wurde. UTF-8 ist heutzutage der Standard und deshalb benutzen wir diese Kodierung. Diese Kodierung wird mithilfe eines Attributs mit dem Namen charset übergeben. Grundsätzlich kann jeder Tag ein Attribut haben, aber es macht nicht bei allen Sinn.

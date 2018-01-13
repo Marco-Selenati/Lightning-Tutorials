2a9720644f324bf5bad544b5024df89b1776c177
Herrald <pascha2525@aol.ch>
2018-01-13 17:26:45 +0100

    X UA Compatible should be the first meta tag

--- php/base.php
+++ php/base.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="de">
   <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="HTML Development" />
     <meta name="author" content="Marco Selenati" />
 

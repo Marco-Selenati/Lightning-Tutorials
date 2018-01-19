bf30d3c6161c555cf7ec072c25050b1992c72f44
Herrald <pascha2525@aol.ch>
2018-01-19 09:39:42 +0100

    Chrome 1 not supported

--- php/base.php
+++ php/base.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="de">
   <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="HTML Development" />

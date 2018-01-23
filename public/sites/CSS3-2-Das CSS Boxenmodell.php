c07d474aef73fbe3793309708db6c00ab95ca5ec
Herrald <pascha2525@aol.ch>
2018-01-23 19:52:48 +0100

    trying to style the video

--- public/sites/CSS3-2-Das CSS Boxenmodell.php
+++ public/sites/CSS3-2-Das CSS Boxenmodell.php
@@ -11,11 +11,12 @@ Da das CSS Boxenmodel ein bisschen schwierig ist, in Worten zu erklären, haben 
 EOD
 );
 $content .= <<<'EOD'
-<video controls>
-    <source src="../video/lernvideo_lightning_tutorials.mp4" type="video/mp4">
-    Dein browser supported nicht das video format.
-</video>
-
+<div class="embed-responsive embed-responsive-16by9">
+    <video controls>
+        <source class="embed-responsive-item" src="../video/lernvideo_lightning_tutorials.mp4" type="video/mp4">
+        Dein browser supported nicht das video format.
+    </video>
+</div>
 EOD;
 $content .= <<<'EOD'
 <a class="btn btn-primary" href="CSS3-1-CSS%20Grundlagen.php" style="color:inherit">Vorheriges</a>
